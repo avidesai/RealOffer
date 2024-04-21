210de46ba2d60974a2ba14df57d41d995e75098d
avidesai <avidesai2@gmail.com>
2024-04-21 12:23:52 -0700

    Added features page

--- realoffer/src/App.js
+++ realoffer/src/App.js
@@ -5,6 +5,7 @@ import LandingPage from './pages/LandingPage/LandingPage';
 import EmailContext from './pages/LandingPage/components/CTA/EmailContext';
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
+import FeaturesPage from './pages/Features/FeaturesPage';
 import './App.css'; // Your global styles
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path='/features' element={<FeaturesPage />} />
           {/* Add other routes here as needed */}
         </Routes>
       </Router>
