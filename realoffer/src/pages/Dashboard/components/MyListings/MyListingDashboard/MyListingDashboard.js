98acdf541e7f3b96f5116166638318b32a386969
avi <avidesai2@gmail.com>
2024-07-07 12:52:30 -0700

    Added spinner animation for mylistingdashboard

--- realoffer/src/pages/Dashboard/components/MyListings/MyListingDashboard/MyListingDashboard.js
+++ realoffer/src/pages/Dashboard/components/MyListings/MyListingDashboard/MyListingDashboard.js
@@ -10,6 +10,7 @@ import './MyListingDashboard.css';
 function MyListingDashboard() {
   const { id } = useParams();
   const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true); // Add loading state
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -17,21 +18,27 @@ function MyListingDashboard() {
       try {
         const response = await axios.get(`http://localhost:8000/api/propertyListings/${id}`);
         setListing(response.data);
+        setLoading(false); // Set loading to false after data is fetched
       } catch (error) {
         console.error('Error fetching listing details:', error);
+        setLoading(false); // Ensure loading is set to false even if there's an error
       }
     };
 
     fetchListingDetails();
   }, [id]);
 
-  if (!listing) {
-    return <div>Loading...</div>;
-  }
-
   const handleBackClick = () => {
     navigate('/dashboard');
   };
+
+  if (loading) {
+    return (
+      <div className="spinner-container">
+        <div className="spinner"></div>
+      </div>
+    );
+  }
 
   return (
     <div className="my-listing-dashboard">
