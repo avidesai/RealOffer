39e2fb42c03c1d69e094c71258b62ac0b3d0cd32
avi <avidesai2@gmail.com>
2024-08-29 15:58:21 -0700

    added auth middleware

---
+++ myapp-backend/middleware/auth.js
@@ -0,0 +1,23 @@
+// middleware/auth.js
+const jwt = require('jsonwebtoken');
+
+module.exports = function(req, res, next) {
+  // Get token from header
+  const token = req.header('Authorization');
+
+  // Check if no token
+  if (!token) {
+    return res.status(401).json({ message: 'No token, authorization denied' });
+  }
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET);
+
+    // Add user from payload
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: 'Token is not valid' });
+  }
+};
