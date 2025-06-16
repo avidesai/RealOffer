17a460091fec5e0112e421154cb200eeffc76cd9
avi <avidesai2@gmail.com>
2025-06-16 03:02:40 -0700

    should fix errors

--- myapp-backend/routes/propertyAnalysis.js
+++ myapp-backend/routes/propertyAnalysis.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getPropertyValuation, getComparableProperties } = require('../controllers/propertyAnalysisController');
+const propertyAnalysisController = require('../controllers/propertyAnalysisController');
 const { authenticateToken } = require('../middleware/auth');
 
 // Get property valuation
-router.get('/valuation/:propertyId', authenticateToken, getPropertyValuation);
+router.get('/valuation/:propertyId', authenticateToken, propertyAnalysisController.getPropertyValuation);
 
 // Get comparable properties
-router.get('/comps/:propertyId', authenticateToken, getComparableProperties);
+router.get('/comps/:propertyId', authenticateToken, propertyAnalysisController.getComparableProperties);
 
 module.exports = router; 
