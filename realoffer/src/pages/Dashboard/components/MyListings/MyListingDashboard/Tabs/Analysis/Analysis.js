79bfbb01385d7c7fc91906f1f830b3f2f51de597
avi <avidesai2@gmail.com>
2025-06-16 03:28:16 -0700

    fixed import line

--- realoffer/src/pages/Dashboard/components/MyListings/MyListingDashboard/Tabs/Analysis/Analysis.js
+++ realoffer/src/pages/Dashboard/components/MyListings/MyListingDashboard/Tabs/Analysis/Analysis.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useAuth } from '../../../../../../../context/AuthContextsgi';
+import { useAuth } from '../../../../../../../context/AuthContext';
 import './Analysis.css';
 
 const Analysis = ({ listingId }) => {
